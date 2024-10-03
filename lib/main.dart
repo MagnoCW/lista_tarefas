37b1a6fd316019d1f358301e46d2393c30b562c7
Magno Evangelista <magnoevangelista@MagnoEvangelistas-Laptop.local>
2024-10-03 16:15:30 -0300

    refatorando

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -31,11 +30,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> tarefas_nao_concluidas = ['1','2','3','4'];
+  List<String> tarefas_nao_concluidas = ['1', '2', '3', '4'];
   List<String> tarefas_concluidas = [];
-  
-  TextEditingController _editingController = TextEditingController();
+
+  final TextEditingController _editingController = TextEditingController();
   String _labelText = 'Digite a nova tarefa';
+  String _alertDialogTitle = 'Adicionar Tarefa';
   int? _editingIndex;
 
   void _changeList(int i, List<String> l1, List<String> l2) {
@@ -51,17 +51,21 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _addTarefa() {
+  void _reset() {
+    _editingController.clear();
+    _labelText = 'Digite a nova tarefa';
+    _alertDialogTitle = 'Adicionar Tarefa';
+    _editingIndex = null;
+  }
+
+  void _addTarefa(List<String> lista) {
     setState(() {
       if (_editingController.text.isNotEmpty) {
         if (_editingIndex != null) {
-          tarefas_nao_concluidas[_editingIndex!] = _editingController.text;
-          _editingIndex = null;
+          lista[_editingIndex!] = _editingController.text;
         } else {
-          tarefas_nao_concluidas.add(_editingController.text);
+          lista.add(_editingController.text);
         }
-        _editingController.clear();
-        _labelText = 'Digite a nova tarefa';
       }
     });
   }
@@ -80,105 +84,147 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _editingController.text = lista[index];
       _labelText = 'Editando a tarefa';
+      _alertDialogTitle = 'Editar Tarefa';
       _editingIndex = index;
     });
+  }
+
+  void _mostrarDialogo(BuildContext context, List<String> lista) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return WillPopScope(
+          onWillPop: () async {
+            _reset();
+            return true;
+          },
+          child: AlertDialog(
+            title: Text(_alertDialogTitle),
+            content: TextField(
+                controller: _editingController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: _labelText,
+                )),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  _addTarefa(lista);
+                  _reset();
+                  Navigator.pop(context);
+                },
+                child: const Text('Salvar'),
+              ),
+              TextButton(
+                onPressed: () {
+                  _reset();
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancelar'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _listas({
+    String titulo = 'Tarefas',
+    required List<String> lista1,
+    required List<String> lista2,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(titulo,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: lista1 == tarefas_concluidas
+                    ? const Color(0xFF388E3C)
+                    : const Color(0xFFD32F2F))),
+        Expanded(
+          child: ReorderableListView(
+            onReorder: (oldIndex, newIndex) {
+              _reorderTarefa(newIndex, oldIndex, lista1);
+            },
+            children: [
+              for (int index = 0; index < lista1.length; index++)
+                Card(
+                  key: ValueKey(index),
+                  surfaceTintColor:
+                      lista1 == tarefas_concluidas ? Colors.green : Colors.red,
+                  child: ListTile(
+                    title: Text(lista1[index]),
+                    leading: IconButton(
+                      icon: lista1 == tarefas_concluidas
+                          ? const Icon(Icons.check_box)
+                          : const Icon(Icons.check_box_outline_blank),
+                      onPressed: () {
+                        _changeList(index, lista1, lista2);
+                      },
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            _editTarefa(index, lista1);
+                            _mostrarDialogo(context, lista1);
+                          },
+                          icon: const Icon(Icons.edit),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            _deleteTarefa(index, lista1);
+                          },
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: const Color(0xFF1E88E5),
+        title: const Text(
+          'Lista de Tarefas',
+          style: TextStyle(
+            fontSize: 24, // Tamanho da fonte
+            fontWeight: FontWeight.bold, // Estilo da fonte
+            color: Colors.white, // Cor da fonte
+          ),
+        ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          SizedBox(height: 5),
-          TextField(
-            controller: _editingController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: _labelText,
-            ),
-          ),
-          SizedBox(height: 20),
-          Text('Tarefas Não Concluídas', style: TextStyle(fontWeight: FontWeight.bold)),
           Expanded(
-            child: ReorderableListView(
-              onReorder: (oldIndex, newIndex) {
-                _reorderTarefa(newIndex, oldIndex, tarefas_nao_concluidas);
-              },
-              children: [
-                for (int index = 0; index < tarefas_nao_concluidas.length; index++)
-                  Card(
-                    key: ValueKey(index),
-                    surfaceTintColor: Colors.red,
-                    child: ListTile(
-                      title: Text(tarefas_nao_concluidas[index]),
-                      leading: IconButton(
-                        icon: Icon(Icons.check_box_outline_blank),
-                        onPressed: () {
-                          _changeList(index, tarefas_nao_concluidas, tarefas_concluidas);
-                        },
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              _editTarefa(index, tarefas_nao_concluidas);
-                            }, icon: Icon(Icons.edit)),
-                          IconButton(
-                            onPressed: () {
-                              _deleteTarefa(index, tarefas_nao_concluidas);
-                            },
-                            icon: Icon(Icons.delete),
-                            color: Colors.red,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-              ]
-            ),
-          ),
-          Text('Tarefas Concluídas', style: TextStyle(fontWeight: FontWeight.bold)),
+              child: _listas(
+                  titulo: 'Tarefas Não Concluídas',
+                  lista1: tarefas_nao_concluidas,
+                  lista2: tarefas_concluidas)),
           Expanded(
-            child: ReorderableListView(
-              onReorder: (oldIndex, newIndex) {
-                _reorderTarefa(newIndex, oldIndex, tarefas_concluidas);
-              },
-              children: [
-                for (int index = 0; index < tarefas_concluidas.length; index++)
-                  Card(
-                    key: ValueKey(index),
-                    surfaceTintColor: Colors.green,
-                    child: ListTile(
-                      title: Text(tarefas_concluidas[index]),
-                      leading: IconButton(
-                        icon: Icon(Icons.check_box),
-                        onPressed: () {
-                          _changeList(index, tarefas_concluidas, tarefas_nao_concluidas);
-                        },
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {
-                          _deleteTarefa(index, tarefas_concluidas);
-                        },
-                        icon: Icon(Icons.delete),
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-              ]
-            ),
-          ),
+              child: _listas(
+                  titulo: 'Tarefas Concluídas',
+                  lista1: tarefas_concluidas,
+                  lista2: tarefas_nao_concluidas)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addTarefa,
-        tooltip: 'Adicionar Tarefa',
+        onPressed: () {
+          _mostrarDialogo(context, tarefas_nao_concluidas);
+        },
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
